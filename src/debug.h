ec6ac8cfe47bd913500eff388b8ce1c9f0fb5944
andrewchambers <andrewchambers@88635e15-b40b-4e14-a348-5361f8d2b4c2>
2011-07-06 12:25:53 +0000

    added printCPU

--- src/debug.h
+++ src/debug.h
@@ -1,2 +1,9 @@
 #include "stdint.h"
+#include "stdio.h"
+#include "z80.h"
 const char * dissasemble(uint8_t b,int secondary);
+
+#define printCPU(cpu) fprintCPU(stdout,cpu)
+
+void
+fprintCPU(FILE*out,z80*pz80);
